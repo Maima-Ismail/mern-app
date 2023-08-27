eef2ae4831f21a293cacfcf18c0a188d45370f57
Maima Ismail <maimamirza@gmail.com>
2023-08-27 19:36:26 +0500

    fourth commit

--- db.js
+++ db.js
@@ -1,13 +1,8 @@
 const mongoose = require('mongoose')
 const db = async () => {
-  try {
-    await mongoose.connect(
-      'mongodb+srv://maimamirza:admin@cluster0.ggjwj3g.mongodb.net/NODE-API?retryWrites=true&w=majority'
-    )
-    console.log('Connected to MongoDB')
-  } catch (error) {
-    console.log(error)
-  }
+  return mongoose.connect(
+    'mongodb+srv://maimamirza:admin@cluster0.ggjwj3g.mongodb.net/NODE-API?retryWrites=true&w=majority'
+  )
 }
 
 module.exports = db
